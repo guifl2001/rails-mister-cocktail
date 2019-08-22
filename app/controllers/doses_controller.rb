041816013c8fd8e195d78d9e199e0527729976f5
guilherme fontana <guifl2001@hotmail.com>
2019-08-22 15:34:55 -0300

    doses controller

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: %i[new create]
+  before_action :set_dose, only: [:destroy]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
